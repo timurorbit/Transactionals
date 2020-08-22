85431a5e56fa7c7a7fa7a3a407d4e8979aca9e19
Timur <t.shamilov.hollow@gmail.com>
2020-08-22 19:28:54 +0300

    exception was removed

--- src/main/java/com/timplant/services/transaction/TransactionNotFoundException.java
+++ src/main/java/com/timplant/services/transaction/TransactionNotFoundException.java
@@ -4,15 +4,6 @@ package com.timplant.services.transaction;
 public class TransactionNotFoundException extends RuntimeException {
 
     /**
-     * Constructs a new runtime exception with {@code null} as its
-     * detail message.  The cause is not initialized, and may subsequently be
-     * initialized by a call to {@link #initCause}.
-     */
-    public TransactionNotFoundException() {
-        super();
-    }
-
-    /**
      * Constructs a new runtime exception with the specified detail message.
      * The cause is not initialized, and may subsequently be initialized by a
      * call to {@link #initCause}.
